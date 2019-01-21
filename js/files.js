c50736f3e983c7f797434b4d1c44d9db371304a5
Fju <flowi@outlook.com>
2019-01-21 21:06:13 +0100

    Load files in directory using Promises

---
+++ js/files.js
@@ -0,0 +1,62 @@
+
+const { dialog } = require('electron').remote;
+
+const fs = require('fs');
+const ffprobe = require('ffprobe');
+const ffprobeStatic = require('ffprobe-static');
+const path = require('path');
+
+
+const SUPPORTED_FILETYPES = [
+	"mp4", "mp3"
+];
+
+const MAX_DEPTH = 8;
+
+
+// returns an array of promises
+function openDirectory() {
+	// open file dialog where the user can select one or multiple directories
+	var dirs = dialog.showOpenDialog({ properties: ['openDirectory', 'multiSelections'] });
+
+	// no directory opened
+	if (!dirs) return;
+	
+	var i, promises = [];
+	for (i = 0; i < dirs.length; ++i) {
+		promises.push(loadDir(dirs[i]));
+	}
+
+	return promises;
+}
+
+
+// It's probably better to rewrite this to only use Promises instead of synchronous and asynchronous functions
+async function loadDir(parent_dir, elements, depth) {
+	// create element array if the functions argument was not given
+	if (!elements) elements = [];
+	if (!depth) depth = 0;
+
+	// iterate through all files and directories inside the current directory
+	var files = fs.readdirSync(parent_dir);
+	for (file of files) {
+		file = parent_dir + '/' + file;
+		var stat = fs.statSync(file);
+		var extension = path.extname(file).substr(1);
+
+		if (stat.isDirectory() && depth <= MAX_DEPTH) {
+			await loadDir(file, elements, depth + 1);
+		} else if (stat.isFile() && SUPPORTED_FILETYPES.indexOf(extension) >= 0) {
+			var info = await ffprobe(file, { path: ffprobeStatic.path });
+			
+			elements.push({
+				birthtime: Math.round(stat.birthtimeMs),
+				duration: parseInt(info.streams[0].duration),
+				path: file,
+				type: extension
+			});
+		}
+	}
+	return elements
+}
+
--- js/ui.js
+++ js/ui.js
@@ -10,41 +10,6 @@ draggableItem.ondragstart = (e) => {
 	ipcRenderer.send('ondragstart', '/test.txt');
 }*/
 
-var fs = require('fs');
-
-const { dialog } = require('electron').remote;
-const ffprobe = require('ffprobe');
-const ffprobeStatic = require('ffprobe-static');
-const path = require('path');
-
-function loadDir(parent_dir, elements) {
-	var files = fs.readdirSync(parent_dir);
-	for (file of files) {
-		file = parent_dir + '/' + file;
-		var extension = path.extname(file);
-		var stat = fs.statSync(file);
-
-		if (stat.isDirectory()) {
-			loadDir(file, elements);
-		} else if (stat.isFile() && extension === '.mp4') {
-			ffprobe(file, { path: ffprobeStatic.path }, function (err, info) {
-				if (err) {
-					console.log(err);
-					return;
-				}				
-				elements.push({
-					birthtime: stat.birthtimeMs,
-					duration: info.streams[0].duration,
-					path: file,
-					type: 'video'
-				});
-			});
-		}
-	}
-	return elements
-}
-
-
 function updatePosition() {
 	var elements = document.querySelectorAll('.content-item'), i;
 	for (i = 0; i != elements.length; ++i) {
@@ -52,8 +17,9 @@ function updatePosition() {
 	}
 }
 
-var scrollContainer = document.querySelector('.content-scroll');
 
+var scrollContainer = document.querySelector('.content-scroll');
+//var bodyContainer = document.querySelector('.content-body');
 var offset = 0;
 
 document.addEventListener('wheel', (e) => {
@@ -64,7 +30,7 @@ document.addEventListener('wheel', (e) => {
 	else if (e.deltaY > 0) offset += 40;
 	
 	// limit offset
-	offset = Math.max(offset, 0);	
+	offset = Math.min(Math.max(offset, -500), 500);	
 	scrollContainer.style.top = offset + 'px';
 
 	// TODO: implement zooming
@@ -72,10 +38,16 @@ document.addEventListener('wheel', (e) => {
 
 });
 document.getElementById('open-dir').addEventListener('click', () => {
-	// open FileDialog for choosing a directory to open
-	var dirs = dialog.showOpenDialog({ properties: ['openDirectory', 'multiSelections'] });
-	if (dirs.length > 0) {
-		var elementList = loadDir(dirs[0], []);
-		console.log(elementList);
-	}
+	openDirectory().forEach(promise => {
+		// create new column
+		
+		var 
+		console.log('Create new column');
+		
+		promise.then(data => {
+			console.log('successful', data);
+		}).catch(err => {
+			console.log('error', err);
+		});			
+	});
 });
