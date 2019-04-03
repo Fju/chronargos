3e0c90824ea9b8ec7766a17490fc490013757cb6
Fju <flowi@outlook.com>
2019-04-03 10:04:14 +0200

    Improved settings box

--- app/src/components/MultipleChoiceInput.vue
+++ app/src/components/MultipleChoiceInput.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="multiple-choice-input" v-show="choices.length > 0">
-		<div class="multiple-choice-item" v-for="(choice, index) in choices">
-			<b>{{ choice.title }}</b>
+		<div class="multiple-choice-item" :class="{ selected: selectedIndex === index }" v-for="(choice, index) in choices" v-on:click="select(index)">
+			<div class="multiple-choice-title">{{ choice.title }}</div>
 			<span>{{ choice.desc }}</span>
 		</div>
 	</div>
@@ -15,6 +15,9 @@
 			}
 		},
 		methods: {
+			select: function(idx) {
+				this.selectedIndex = idx;				
+			},
 			getSelected: function() {
 				return '';			 
 			} 
--- app/src/components/SettingsOverlay.vue
+++ app/src/components/SettingsOverlay.vue
@@ -2,22 +2,40 @@
 	<div class="settings-overlay" v-show="visible">
 		<div class="settings-cancel-area" v-on:click="visible = false"></div>
 		<div class="settings-box">
-			<div class="settings-title">Preferences</div>
-			<p>Will be implemented soon.</p>
-			<multiple-choice-input :choices="choices"></multiple-choice-input>
-			<button>Test</button>
+			<div class="settings-main">
+				<h1>Preferences</h1>
+				<p>
+					<h2>Dock mode</h2>
+					<multiple-choice-input :choices="dock_choices"></multiple-choice-input>
+				</p>
+				
+				<p>
+					<h2>Dock on display</h2>
+					<multiple-choice-input :choices="display_choices"></multiple-choice-input>
+				</p>
+			</div>
+			<div class="settings-button-group">
+				<button>Apply changes</button>
+				<button>Restore defaults</button>
+			</div>
 		</div>
 	</div>
 </template>
 <script>
 	import MultipleChoiceInput from './MultipleChoiceInput.vue';
+	import dock from '../js/dock.js';
 
 	export default {
 		data() {
 			return {
-				choices: [
+				dock_choices: [
 					{ title: 'Don\'t dock', desc: 'Position window manually where you want it to be' },
-		   			{ title: 'Dock right', desc: 'Window is locked to the right side of the screen' }				
+		   			{ title: 'Dock right', desc: 'Window is locked to the right side of the screen' },
+					{ title: 'Dock left', desc: 'Window is locked to the left side of the screen'}			
+				],
+				display_choices: [
+					{ title: 'Display 0 (Primary)', desc: '' },
+					{ title: 'Display 1', desc: '' }
 				],
 				visible: false
 			};
